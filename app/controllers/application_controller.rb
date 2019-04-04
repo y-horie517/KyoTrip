d09fa8d26dc3918c30e4cede7a4e442c790aa4f3
y-horie517 <y.horie0517@gmail.com>
2019-04-04 07:19:00 +0000

    [Add]ユーザ新規登録時のユーザ名登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	# ユーザ登録時に指定したパラメタを登録できるようにするため
+  	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  	def configure_permitted_parameters
+    #strong parametersを設定し、usernameを許可
+    	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    	# devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
+  	end
+
 end
