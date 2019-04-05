62c09bcbeea3bb926f5eaed41c7b4461a63b8c5e
y-horie517 <y.horie0517@gmail.com>
2019-04-05 16:07:10 +0000

    [Craeate]マイページ内にマップ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'top/index'
+  get 'top' => 'top#top'
   root to: 'top#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
