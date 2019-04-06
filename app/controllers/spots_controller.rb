a42ded5fb92f7c913d65caac48ab695b19e8a401
y-horie517 <y.horie0517@gmail.com>
2019-04-06 06:05:49 +0000

    [Create]スポット登録機能

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,9 +2,30 @@ class SpotsController < ApplicationController
   def index
   end
 
+  def new
+  	@spot = Spot.new
+  end
+
   def create
+  	spot = Spot.new(spot_params)
+  	spot.user_id = current_user.id
+  	# DBへ保存する
+    if spot.save!
+     	flash[:notice] = "スポットが1件登録されました。"
+     	redirect_to spot_path(spot.id)
+    else
+     	flash[:warning] = "スポットの登録に失敗しました。未入力の項目があります。"
+     	render new_spot_path
+    end
   end
 
   def show
+  	@spot = Spot.find(params[:id])
+  end
+
+
+  private
+  def spot_params
+  	params.require(:spot).permit(:name, :description, :address, :url, :category_id, :spotimage)
   end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,7 @@
 class Spot < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
+
+	# Active Storageを用いたプロフィール画像用
+	has_one_attached :spotimage
 end
