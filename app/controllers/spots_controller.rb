7c684255fac5017bb8594dc4bf202fa44d75302f
y-horie517 <y.horie0517@gmail.com>
2019-04-06 07:13:49 +0000

    [Update]スポット編集可能なユーザを制限

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -16,12 +16,28 @@ class SpotsController < ApplicationController
      	redirect_to spot_path(spot.id)
     else
      	flash[:warning] = "スポットの登録に失敗しました。未入力の項目があります。"
-     	render new_spot_path
+     	render new_spot_path(spot.id)
     end
   end
 
   def show
   	@spot = Spot.find(params[:id])
+  end
+
+  def edit
+  	@spot = Spot.find(params[:id])
+  end
+
+  def update
+  	spot = Spot.find(params[:id])
+  	# DBへ保存する
+    if spot.update(spot_params)
+     	flash[:notice] = "情報が更新されました。"
+     	redirect_to spot_path(spot.id)
+    else
+     	flash[:warning] = "情報の更新に失敗しました。未入力の項目があります。"
+     	render edit_spot_path(spot.id)
+    end
   end
 
 
