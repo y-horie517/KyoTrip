11db8facb9150e23a29764f85ba4d99217bcb8ce
y-horie517 <y.horie0517@gmail.com>
2019-04-08 12:34:28 +0000

    [Create]Favoriteモデル

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,12 +3,12 @@ class Spot < ApplicationRecord
 	belongs_to :category
 
 	has_many :reviews, dependent: :destroy
-	
+	has_many :favorites, dependent: :destroy
 	# Active Storageを用いた画像投稿用
 	has_one_attached :spotimage
 
 	# ユーザがお気に入りに登録しているか判定
-	# def favorited_by?(user)
- #    	favorites.where(user_id: user.id).exists?
- #  	end
+	def favorited_by?(user)
+    	favorites.where(user_id: user.id).exists?
+  	end
 end
