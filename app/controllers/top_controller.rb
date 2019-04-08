f2865ad387cf526269078b13eb25b28a6c2ea7f1
y-horie517 <y.horie0517@gmail.com>
2019-04-08 07:56:17 +0000

    [Create]メニュー画面に最新のレビュー投稿を表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
   def index
+  	# 最新のレビューを５件取得
+  	@reviews = Review.all.order(created_at: "DESC").limit(5)
   	render :layout => "application"
   	# render :layout => "top"
   end
