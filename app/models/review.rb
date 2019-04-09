dfcf4386ce7a74c54f2d08106ce356c4ad3015f5
y-horie517 <y.horie0517@gmail.com>
2019-04-09 14:36:29 +0000

    [Update]文字数カウント機能作成

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,4 +10,21 @@ class Review < ApplicationRecord
 
   	# Active Storageを用いた画像投稿用(複数枚)
 	has_many_attached :reviewimages
+
+	# validate :validate_img
+
+ #  def validate_img
+ #    return unless reviewimages.attached?
+ #    if reviewimages.blob.byte_size > 50.megabytes
+ #      reviewimages.purge
+ #      errors.add(:icon, I18n.t('errors.messages.file_too_large')
+ #    elsif !image?
+ #      reviewimages.purge
+ #      errors.add(:icon, I18n.t('errors.messages.file_type_not_image'))
+ #    end
+ #  end
+
+ #  def image?
+ #    %w[image/jpg image/jpeg image/gif image/png].include?(reviewimages.blob.content_type)
+ #  end
 end
