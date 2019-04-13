bd918780bb244877c5a4c3f443d7a5bed6f0edf5
y-horie517 <y.horie0517@gmail.com>
2019-04-13 05:36:05 +0000

    [Create]スライドショー機能

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   $('.topic-area').slick({
   	 // アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
  	accessibility: true,
+ 	 // 自動再生。trueで自動再生される。
+  	autoplay: true,
     dots: true,
     slidesToShow: 5,
     slidesToScroll: 1,
