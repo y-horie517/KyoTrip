5fd08148477e4c554fb35b3159ad65f66844cd1f
y-horie517 <y.horie0517@gmail.com>
2019-04-16 10:29:26 +0000

    [Create]トップページでのエリア検索機能

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,6 +2,7 @@ class SpotsController < ApplicationController
 
   def index
     @search = Spot.ransack(params[:q]) #ransackメソッド推奨
+    # @search = Spot.ransack(params.fetch(:q, category_id_eq: 2))
   	@spots = @search.result.page(params[:page])
     if user_signed_in?
       @user = current_user
@@ -15,6 +16,27 @@ class SpotsController < ApplicationController
   def create
   	spot = Spot.new(spot_params)
   	spot.user_id = current_user.id
+
+    # area_idを付与
+    if spot.address.match?(/上京区|中京区|下京区/)
+      spot.area_id = 1
+    elsif spot.address.match?(/北山区/)
+      spot.area_id = 2
+    elsif spot.address.match?(/左京区|東山区|山科区/)
+      spot.area_id = 3
+    elsif spot.address.match?(/右京区|西京区/)
+      spot.area_id = 4
+    elsif spot.address.match?(/南区|伏見区/)
+      spot.area_id = 5
+    # 京都市という文字列がない場合は市外と判断
+    elsif !spot.address.match?(/京都市/)
+      spot.area_id = 6
+    # 住所未入力の場合
+    else
+      # その他を念のため用意
+      spot.area_id = 7
+    end
+
   	# DBへ保存する
     if spot.save
      	flash[:notice] = "スポットが1件登録されました。"
@@ -43,6 +65,25 @@ class SpotsController < ApplicationController
 
   def update
   	spot = Spot.find(params[:id])
+    # area_idを付与
+    if spot.address.match?(/上京区|中京区|下京区/)
+      spot.area_id = 1
+    elsif spot.address.match?(/北山区/)
+      spot.area_id = 2
+    elsif spot.address.match?(/左京区|東山区|山科区/)
+      spot.area_id = 3
+    elsif spot.address.match?(/右京区|西京区/)
+      spot.area_id = 4
+    elsif spot.address.match?(/南区|伏見区/)
+      spot.area_id = 5
+    # 京都市という文字列がない場合は市外と判断
+    elsif !spot.address.match?(/京都市/)
+      spot.area_id = 6
+    # 住所未入力の場合
+    else
+      # その他を念のため用意
+      spot.area_id = 7
+    end
   	# DBへ保存する
     if spot.update(spot_params)
      	flash[:notice] = "情報が更新されました。"
