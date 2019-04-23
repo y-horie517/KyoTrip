8b6dfdbe6d197cb290f3a9f65a8a9789b00bd071
y-horie517 <y.horie0517@gmail.com>
2019-04-23 09:33:36 +0000

    [Fix]マイページでの画像登録時のバリデーション追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,6 +11,7 @@ class Review < ApplicationRecord
   	# Active Storageを用いた画像投稿用(複数枚)
 	has_many_attached :reviewimages
 	validate :reviewimages_length
+	# validate :validate_icon
 
 
 	# 一覧表示のページング
@@ -29,20 +30,20 @@ class Review < ApplicationRecord
     	
   	end
 
-	# validate :validate_img
 
- #  def validate_img
- #    return unless reviewimages.attached?
- #    if reviewimages.blob.byte_size > 50.megabytes
- #      reviewimages.purge
- #      errors.add(:icon, I18n.t('errors.messages.file_too_large')
- #    elsif !image?
- #      reviewimages.purge
- #      errors.add(:icon, I18n.t('errors.messages.file_type_not_image'))
- #    end
- #  end
 
- #  def image?
- #    %w[image/jpg image/jpeg image/gif image/png].include?(reviewimages.blob.content_type)
- #  end
+  # def validate_img
+  #   return unless reviewimages.attached?
+  #   if reviewimages.blob.byte_size > 50.megabytes
+  #     reviewimages.purge
+  #     errors.add(:icon, I18n.t('errors.messages.file_too_large')
+  #   elsif !image?
+  #     reviewimages.purge
+  #     errors.add(:icon, I18n.t('errors.messages.file_type_not_image'))
+  #   end
+  # end
+
+  # def image?
+  #   %w[image/jpg image/jpeg image/gif image/png].include?(reviewimages.blob.content_type)
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,28 @@ class User < ApplicationRecord
 
 	# Active Storageを用いたプロフィール画像用
 	has_one_attached :userimage
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	       :recoverable, :rememberable, :validatable
 
-    
+	validates :username, presence: true
+    validate :validate_prof
+
+    def validate_prof
+    	return unless userimage.attached?
+
+    	if userimage.blob.byte_size > 10.megabytes
+      		userimage.purge
+	      	errors.add(:userimage, I18n.t('errors.messages.file_too_large'))
+	    elsif !image?
+	      	userimage.purge
+	      	errors.add(:userimage, I18n.t('errors.messages.file_type_not_image'))
+	    end
+	  end
+
+	def image?
+    	%w[image/jpg image/jpeg image/gif image/png].include?(userimage.blob.content_type)
+  	end
 end
+
